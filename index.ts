9419fc8d6ae3ec838d1330587c754a08297a96c8
Christian <christian.bedonv@gmail.com>
2022-05-30 12:40:56 -0500

    feat: get skills by occupation Id

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import neo4j, { Driver, Session } from 'neo4j-driver';
 import { toNativeTypes } from './utils.js';
+import { int } from 'neo4j-driver'
 
 dotenv.config();
 
@@ -10,30 +11,39 @@ const port = process.env.PORT || '8000';
 const neoUri = process.env.NEO_URI || '';
 const neoUsername = process.env.NEO_USERNAME || '';
 const neoPassword = process.env.NEO_PASSWORD || '';
-const driver: Driver = neo4j.driver(neoUri, neo4j.auth.basic(neoUsername, neoPassword));
-const session: Session = driver.session();
+const occupation = 'Desarrollador Backend Javascript';
+const occupationId = 3322;
 
-try {
-  const result = await session.run(
-    `
-    MATCH (tom:Person {name: "Tom Hanks"})-[:ACTED_IN]->(tomHanksMovies)
-    RETURN tom,tomHanksMovies
-    `,
-  )
+const getSkillsByOccupationId = async () => {
+  const driver: Driver = neo4j.driver(neoUri, neo4j.auth.basic(neoUsername, neoPassword));
 
-  const movies = result.records.map(row => toNativeTypes(row.get('tomHanksMovies')))
+  const session: Session = driver.session();
+ try {
+    const result = await session.run(
+      `
+        MATCH (occ:occupation)-[r:occupation_skill_mandatory]->(n2)
+        WHERE ID(occ) = $occupationId
+        RETURN occ, r, n2
+      `, { occupationId: int(occupationId) }
+    )
 
-  console.log(movies);
 
-} finally {
-  await session.close()
+    const mandatorySkills = result.records.map(row => toNativeTypes(row.get('r')));
+    console.log(mandatorySkills)
+    return mandatorySkills
+
+  } finally {
+    await session.close()
+  }
+
+
 }
 
-// on application exit:
-await driver.close()
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server is running');
+app.get('/', async (req: Request, res: Response) => {
+  const skills = await getSkillsByOccupationId();
+
+  res.send(skills);
 });
 
 app.listen(port, () => {
